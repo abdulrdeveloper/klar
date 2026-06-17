5c60bac5cce530e5da5b4d81d0529f69564cc545
abdulrdeveloper <abdulrahman2040b@gmail.com>
2026-06-17 13:23:09 +0500

    feat: built auth frontend login and register page

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+const Forget = () => {
+  return (
+    <div>Forget Password will be Here</div>
+  )
+}
+
+export default Forget
---
+++ app/verify-email/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+const Verify = () => {
+  return (
+    <div>Verify Your Email</div>
+  )
+}
+
+export default Verify
